599f5a4ea98ba8ca43e84d295a38fdde0659342e
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-11-18 17:43:21 -0300

    Alterando a controller para receber os query params.

--- src/app/controllers/CompanyController.js
+++ src/app/controllers/CompanyController.js
@@ -1,6 +1,6 @@
 module.exports = {
   async show(request, response) {
-    const { uf, empresa } = { uf: 'ms', empresa: 'sanesul' };
+    const { uf, empresa } = request.params;
     const dados = await require(`../database/${uf}/${empresa}.json`);
 
     return response.status(200).json(dados);
